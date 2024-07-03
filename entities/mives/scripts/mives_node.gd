6b915c00a8c43926a5f23af79d0180158916ec22
bedroomvampire <leannetoni@proton.me>
2024-07-04 07:47:12 +0900

    some stuff

---
+++ entities/mives/scripts/mives_node.gd
@@ -0,0 +1,12 @@
+extends Node
+
+@export_range(0, 100) var test_motive : float = 82.5
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
