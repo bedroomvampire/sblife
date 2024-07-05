33ed234c5a1e057e99d02bfa8a01612066f426da
bedroomvampire <leannetoni@proton.me>
2024-07-05 16:28:32 +0900

    building wall prototype test 1
    scaling in negatives is a little odd and diagonal walls arent added yet (still havent figured out rotation)

--- scenes/placeholders/wall_test.gd
+++ scenes/placeholders/wall_test.gd
@@ -1,6 +1,8 @@
 extends Node3D
 
 var hit : Dictionary
+var posit_set : bool
+var posit : Vector3
 @export var camera : CharacterBody3D
 @onready var wall_test = $MeshInstance3D
 
@@ -16,11 +18,29 @@ func _process(_delta):
 	var query = PhysicsRayQueryParameters3D.create(origin, camera.camera.floor_point_from_mouse_position())
 	hit = ray.intersect_ray(query)
 	if hit:
-		var pos = snapped(hit.position, Vector3(1,1,1))
 		var init_pos : Vector3
 		#print(pos)
 		if Input.is_action_pressed("left_click"):
-			init_pos = pos
+			var pos = snapped(hit.position, Vector3(1,1,1))
+			if !posit_set:
+				init_pos = pos
+				_posit(init_pos)
+				print("A:")
+				print(init_pos)
+				posit_set = true
 		if Input.is_action_just_released("left_click"):
-			var final_result = snapped((init_pos - pos), Vector3(1,1,1))
+			var pos = snapped(hit.position, Vector3(1,1,1))
+			print("A:")
+			print(posit)
+			print("B:")
+			var final_result = snapped((pos - posit), Vector3(1,1,1))
 			print(final_result)
+			wall_test.position = posit + (final_result / 2) + Vector3(0, 1.5, 0)
+			wall_test.scale = (final_result * 4) + Vector3(1,1,1)
+			#print("C:")
+			#var wall_rot = Quaternion(pos, -posit)
+			#print(wall_rot)
+			posit_set = false
+
+func _posit(A):
+	posit = A
